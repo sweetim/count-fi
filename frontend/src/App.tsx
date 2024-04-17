b9c210838ab2350cd36a3c1cfbcdb6e9e5e10a35
sweetim <hosweetim@gmail.com>
2024-04-17 23:09:18 +0900

    fix layout issue in mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,27 @@
 import { WalletSelector } from '@aptos-labs/wallet-adapter-ant-design';
-import { Col, Flex, Layout, Row } from 'antd'
+import { Col, Flex, Grid, Layout, Row } from 'antd'
 import { Content, Header } from 'antd/es/layout/layout'
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { CounterRecord, getAllRecords } from './module/counter/contract';
 import { ActionCounter, RecordTimeline } from './module/counter/components';
 import CounterLogo from './icons/CounterLogo';
 
+const { useBreakpoint } = Grid;
+
 const App: FC = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [ height, setHeight ] = useState(0)
+
   const [allRecords, setAllRecords] = useState<CounterRecord[]>([])
 
   useEffect(() => {
     getAllRecords().then(result => setAllRecords(result))
   }, [])
+
+  const screens = useBreakpoint()
+  useEffect(() => {
+    setHeight(screens.xs && ref.current?.clientHeight || 0)
+  }, [screens.xs])
 
   return (
     <Layout style={{ height: "100vh" }}>
@@ -22,21 +32,21 @@ const App: FC = () => {
           <CounterLogo style={{ width: "32px", fill: "white " }} />
           <WalletSelector />
         </Flex>
-
       </Header>
       <Content>
         <Row
           style={{ height: "100%" }}>
           <Col
+            ref={ref}
             xs={24}
-            md={8}>
+            md={{ flex: "auto" }}>
             <ActionCounter />
           </Col>
-          <Col xs={24}
+          {allRecords.length > 0 && <Col xs={24}
             md={16}
-            style={{ overflow: "auto", padding: "20px", height: "100%" }}>
+            style={{ overflow: "auto", padding: "20px", height: `calc(100% - ${height}px` }}>
             <RecordTimeline records={allRecords} />
-          </Col>
+          </Col>}
         </Row>
       </Content>
     </Layout>
