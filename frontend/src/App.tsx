6b5cff3bd1165acb98d4167e8599f171d32b4769
sweetim <hosweetim@gmail.com>
2024-04-17 23:57:35 +0900

    change layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { WalletSelector } from '@aptos-labs/wallet-adapter-ant-design';
 import { Col, Flex, Grid, Layout, Row } from 'antd'
 import { Content, Header } from 'antd/es/layout/layout'
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { CounterRecord, getAllRecords } from './module/counter/contract';
 import { ActionCounter, RecordTimeline } from './module/counter/components';
 import CounterLogo from './icons/CounterLogo';
@@ -9,9 +9,6 @@ import CounterLogo from './icons/CounterLogo';
 const { useBreakpoint } = Grid;
 
 const App: FC = () => {
-  const ref = useRef<HTMLDivElement>(null)
-  const [ height, setHeight ] = useState(0)
-
   const [allRecords, setAllRecords] = useState<CounterRecord[]>([])
 
   useEffect(() => {
@@ -19,9 +16,36 @@ const App: FC = () => {
   }, [])
 
   const screens = useBreakpoint()
-  useEffect(() => {
-    setHeight(screens.xs && ref.current?.clientHeight || 0)
-  }, [screens.xs, allRecords])
+
+  const renderMobile = () => {
+    const getHeight = allRecords.length === 0 ? "100%" : "auto"
+
+    return (
+      <>
+        <Row style={{ paddingTop: "0.25rem", paddingBottom: "0.25rem", height: `${getHeight}` }}>
+          <ActionCounter />
+        </Row>
+        {allRecords.length > 0 && <Row style={{ height: "100%", overflow: "auto", padding: "20px" }}>
+          <RecordTimeline records={allRecords} />
+        </Row>}
+      </>
+    )
+  }
+
+  const renderDesktop = () => {
+    return (
+      <Row
+        style={{ height: "100%" }}>
+        <Col sm={{ flex: "auto" }}>
+          <ActionCounter />
+        </Col>
+        {allRecords.length > 0 && <Col span={16}
+          style={{ overflow: "auto", padding: "20px", height: "100%" }}>
+          <RecordTimeline records={allRecords} />
+        </Col>}
+      </Row>
+    )
+  }
 
   return (
     <Layout style={{ height: "100vh" }}>
@@ -33,22 +57,8 @@ const App: FC = () => {
           <WalletSelector />
         </Flex>
       </Header>
-      <Content>
-        <Row
-          style={{ height: "100%" }}>
-          <Col
-            ref={ref}
-            xs={24}
-            md={{ flex: "auto" }}
-            style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem"}}>
-            <ActionCounter />
-          </Col>
-          {allRecords.length > 0 && <Col xs={24}
-            md={16}
-            style={{ overflow: "auto", padding: "20px", height: `calc(100% - ${height}px` }}>
-            <RecordTimeline records={allRecords} />
-          </Col>}
-        </Row>
+      <Content style={{ display: "flex", flexDirection: "column" }}>
+        { screens.xs ? renderMobile() : renderDesktop() }
       </Content>
     </Layout>
   )
