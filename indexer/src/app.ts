7ca5e0a3b582a3428ba97faea7b91801ad9f8f35
sweetim <hosweetim@gmail.com>
2024-04-20 00:21:24 +0900

    change to use env var

--- indexer/src/app.ts
+++ indexer/src/app.ts
@@ -8,36 +8,26 @@ import "dotenv/config"
 
 const {
   GRPC_STREAM_API_KEY,
-  ABLY_API_KEY
+  GRPC_STREAM_ENDPOINT_URL,
+  ABLY_API_KEY,
+  APTOS_COUNTER_MODULE_ADDRESS
 } = process.env
 
-const GRPC_STREAM_ENDPOINT_URL = "grpc.devnet.aptoslabs.com:443"
 const ABLY_APTOS_COUNTER_CHANNEL_NAME = "aptos-counter"
-const APTOS_COUNTER_MODULE_ADDRESS = "0x6b2cf48e40e3b651c309dc444d0d094ed9c342089289c8bf50c4a5646271f20b"
 
 const aptosClient = new Aptos(new AptosConfig({
   network: Network.TESTNET
 }))
 
 const client = new aptos.indexer.v1.RawDataClient(
-  GRPC_STREAM_ENDPOINT_URL,
+  GRPC_STREAM_ENDPOINT_URL!,
   ChannelCredentials.createSsl(),
   {
     // "grpc.keepalive_permit_without_calls": 1,
     "grpc.keepalive_time_ms": 10000000,
-    // 0 - No compression
-    // 1 - Compress with DEFLATE algorithm
-    // 2 - Compress with GZIP algorithm
-    // 3 - Stream compression with GZIP algorithm
     "grpc.default_compression_algorithm": 2,
-    // 0 - No compression
-    // 1 - Low compression level
-    // 2 - Medium compression level
-    // 3 - High compression level
     "grpc.default_compression_level": 3,
-    // -1 means unlimited
     "grpc.max_receive_message_length": -1,
-    // -1 means unlimited
     "grpc.max_send_message_length": -1,
   },
 )
@@ -76,7 +66,7 @@ const fastify = Fastify({
 
         const events = transaction.user?.events || []
         for (const ev of events) {
-          if (ev.typeStr?.includes(APTOS_COUNTER_MODULE_ADDRESS)) {
+          if (ev.typeStr?.includes(APTOS_COUNTER_MODULE_ADDRESS!)) {
             await channel.publish(ev)
             console.log(ev)
           }
